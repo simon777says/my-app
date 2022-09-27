b31a12de3c0b0977b0a02bfba0b6f0ac6c75e515
simon777says <76588744+simon777says@users.noreply.github.com>
2022-09-27 23:07:41 +0300

    51 51

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,60 +1,15 @@
 import React from "react";
-import css from "./Friends.module.scss"
-
+import css from "./Friends.module.scss";
+import * as axios from "axios";
+import ava from"./../../img/avatar-vector-icon-png_296057.jpg"
 const Friends = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: true,
-                fullName: "Ben",
-                status: "online",
-                photoUrl:
-                  "https://png.pngtree.com/element_our/png_detail/20181228/avatar-vector-icon-png_296057.jpg",
-                location: {
-                  city: "Lviv",
-                  country: "Ukraine",
-                },
-              },
-              {
-                id: 2,
-                followed: false,
-                fullName: "gsaafff",
-                status: "online",
-                photoUrl:
-                  "https://png.pngtree.com/element_our/png_detail/20181228/avatar-vector-icon-png_296057.jpg",
-                location: {
-                  city: "Lviv",
-                  country: "Ukraine",
-                },
-              },
-              {
-                id: 3,
-                followed: true,
-                fullName: "авпgварen",
-                status: "online",
-                photoUrl:
-                  "https://png.pngtree.com/element_our/png_detail/20181228/avatar-vector-icon-png_296057.jpg",
-                location: {
-                  city: "Lviv",
-                  country: "Ukraine",
-                },
-              },
-              {
-                id: 4,
-                followed: false,
-                fullName: "Bfваen",
-                status: "online",
-                photoUrl:
-                  "https://png.pngtree.com/element_our/png_detail/20181228/avatar-vector-icon-png_296057.jpg",
-                location: {
-                  city: "Lviv",
-                  country: "Ukraine",
-                },
-              },
-            ])}
-
+  if (props.users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      
+      props.setUsers(response.data.items);
+    })
+  }
 
     return (
         <div className={css}>
@@ -62,23 +17,38 @@ const Friends = (props) => {
                 <div key={u.id} >
                     <span>
                         <div>
-                            <img className={css.ava} src={u.photoUrl}/>
+                            <img className={css.ava} src={u.photos.small !=null ? u.photos.small : ava}/>
                         </div>
                         <div>{u.followed ? <button className={css.unfollow}
                             onClick={() => { props.unfollow(u.id);}} >Unfollow</button>
                             : <button className={css.follow} onClick={() => { props.follow(u.id);}}>Follow</button>}</div>
                     </span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status }</div>
                     </span>
                     <span>
-                        <div>{ u.location.city}</div>
-                        <div>{ u.location.country}</div>    
+                        {/* <div>{ u.location.city}</div> */}
+                        {/* <div>{ u.location.country}</div>     */}
                     </span>
                 </div>
                 )}
       </div>
     )
 };
-export default Friends;
+  export default Friends;
+
+  
+// "items"= [
+//   {
+//     "name": "noonemassive",
+//     "id": 26061,
+//     "uniqueUrlName": null,
+//     "photos": {
+//       "small": null,
+//       "large": null
+//     },
+//     "status": null,
+//     "followed": false
+//   },
+
